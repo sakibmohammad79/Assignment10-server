8a2c5aca6b32ed09ddb5486b70b7d9bc4ea3fc80
Mohammad Sakib <mohammadsakib7679@gmail.com>
2023-05-02 16:39:01 +0600

    recipe all data are added

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.get('/recipe', (req, res) =>{
     res.send(recipe)
 });
 
+app.get('/recipe/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedRecipe = recipe.find(singleRecipe => singleRecipe.id == id);
+    res.send(selectedRecipe);
+
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
